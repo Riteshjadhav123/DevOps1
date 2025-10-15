06b96b4b637d5e2f13e64e6f9d2d41ce64ec2416
Riteshjadhav123 <riteshsjadhav302@gmail.com>
2025-10-15 11:43:03 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 ﻿public class HelloWorld { 
    public static void main(String[] args) {
-     System.out.println("Hello from DevOps practical!");
+     System.out.println("Hello from DevOps practical");
    }
  }
+
 
